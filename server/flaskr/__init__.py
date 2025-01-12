36537d2019cbb37d09a631db88441778874d5de7
Lucius Holzinger <luciholzinger@gmail.com>
2025-01-12 11:03:58 -0800

    formatted python file for further consistency
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -4,18 +4,10 @@ from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    cors = CORS(app, origins='*')
+    cors = CORS(app, origins="*")
 
-    @app.route("/api/users", methods=['GET'])
+    @app.route("/api/users", methods=["GET"])
     def users():
-        return jsonify(
-            {
-                "users": [
-                    'lucius',
-                    'gabe',
-                    'jai'
-                ]
-            }
-        )
+        return jsonify({"users": ["lucius", "gabe", "jai"]})
 
     return app
