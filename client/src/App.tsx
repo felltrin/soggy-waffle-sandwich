e99ffde7284c8107d21ab25a5649d79c0dcd1dcb
Lucius Holzinger <luciholzinger@gmail.com>
2025-01-12 10:42:28 -0800

    tailwindcss is configured so that we can have consistent styling across the app
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [array, setArray] = useState([]);
 
   const fetchAPI = async () => {
@@ -19,31 +16,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-          {
-            array.map((user, index) => (
-              <div key={index}>
-                <span>{user}</span>
-                <br/>
-              </div>
-            ))
-          }
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-5xl font-bold underline">
+        Hello World!
+      </h1>
+      {
+        array.map((user, index) => (
+          <div key={index}>
+            <span>{user}</span>
+            <br/>
+          </div>
+        ))
+      }
     </>
   )
 }
