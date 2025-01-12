7c5fe0fc30179da2c0367f92dce57667e3df4226
Lucius Holzinger <luciholzinger@gmail.com>
2025-01-12 00:38:00 -0800

    the stage is set. now perform
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

---
+++ server/flaskr/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    cors = CORS(app, origins='*')
+
+    @app.route("/api/users", methods=['GET'])
+    def users():
+        return jsonify(
+            {
+                "users": [
+                    'lucius',
+                    'gabe',
+                    'jai'
+                ]
+            }
+        )
+
+    return app
