d150151ef52692e403dd24c05fb323ea1a64c198
Lucius Holzinger <luciholzinger@gmail.com>
2025-01-17 02:09:17 -0800

    login and register endpoints set to be implemented in the frontend
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -2,15 +2,19 @@ import os
 
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        JWT_SECRET_KEY='a72068d06c2c4ea18de0510df1d7b9a600fdc215010dedec4d85c59f0a743f21',
+        JWT_TOKEN_LOCATION='headers'
     )
     cors = CORS(app, origins="*")
+    jwt = JWTManager(app)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -28,5 +32,8 @@ def create_app(test_config=None):
 
     from  . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
---
+++ server/flaskr/auth.py
@@ -0,0 +1,70 @@
+from flask import (
+    Blueprint, redirect, request, url_for, jsonify
+)
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        print('Received data:', username, password)
+        db = get_db()
+
+        if not username:
+            return jsonify({'message': 'Registration Failed: Username is required.'}), 401
+        elif not password:
+            return jsonify({'message': 'Registration Failed: Password is required.'}), 401
+
+        try:
+            db.execute(
+                "INSERT INTO user (username, password) VALUES (?, ?)",
+                (username, generate_password_hash(password)),
+            )
+            db.commit()
+        except db.IntegrityError:
+            return jsonify({'message': f"Username {username} is already registered."}), 401
+        else:
+            return redirect(url_for("auth.login"))
+
+
+@bp.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        print('Received data:', username, password)
+
+        db = get_db()
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user and check_password_hash(user['password'], password):
+            access_token = create_access_token(identity=user['id'])
+            return jsonify({'message': 'Login Success', 'access_token': access_token})
+        else:
+            return jsonify({'message': 'Login Failed'}), 401
+
+
+@bp.route('/get_name', methods=['GET'])
+@jwt_required
+def get_name():
+    user_id = get_jwt_identity()
+    db = get_db()
+    user = db.execute(
+        'SELECT * FROM user WHERE id = ?', (user_id,)
+    ).fetchone()
+
+    if user:
+        return jsonify({'message': 'User found', 'name': 'User ' + user['username']})
+    else:
+        return jsonify({'message': 'User not found'}), 404
