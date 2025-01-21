5746938d0f3a6b3a32f272e389d59925e023856b
Lucius Holzinger <luciholzinger@gmail.com>
2025-01-20 18:14:56 -0800

    The update endpoint has been implemented to allow the user to update the workouts if they so desire.
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- server/flaskr/workout.py
+++ server/flaskr/workout.py
@@ -1,6 +1,4 @@
-from flask import (
-    Blueprint, redirect, request, url_for, jsonify
-)
+from flask import Blueprint, redirect, request, url_for, jsonify
 from werkzeug.exceptions import abort
 
 from flask_jwt_extended import jwt_required, current_user
@@ -17,12 +15,17 @@ def index():
         "SELECT w.id, created, distance, duration, user_id"
         " FROM workout w WHERE w.user_id = ?"
         "  ORDER BY created DESC",
-        (current_user["id"],)
+        (current_user["id"],),
     ).fetchall()
 
     workout_arr = []
     for workout in workouts:
-        workout_obj = {"created": workout["created"], "duration": workout["duration"], "distance": workout["distance"]}
+        workout_obj = {
+            "id": workout["id"],
+            "created": workout["created"],
+            "duration": workout["duration"],
+            "distance": workout["distance"],
+        }
         workout_arr.append(workout_obj)
 
     return jsonify({"message": "Workouts Retrieved", "workouts": workout_arr})
@@ -41,9 +44,55 @@ def create():
         else:
             db = get_db()
             db.execute(
-                "INSERT INTO workout (duration, distance, user_id)"
-                " VALUES (?, ?, ?)",
-                (duration, distance, current_user["id"])
+                "INSERT INTO workout (duration, distance, user_id)" " VALUES (?, ?, ?)",
+                (duration, distance, current_user["id"]),
             )
             db.commit()
             return redirect(url_for("workout.index"))
+
+
+def get_workout(id, check_author=True):
+    workout = (
+        get_db()
+        .execute(
+            "SELECT w.id, duration, distance, created, user_id"
+            " FROM workout w JOIN user u ON w.user_id = u.id"
+            " WHERE w.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if workout is None:
+        abort(404, f"Workout id {id} doesn't exist.")
+
+    if check_author and workout["user_id"] != current_user["id"]:
+        abort(403)
+
+    return workout
+
+
+@bp.route("/<int:id>/update", methods=["POST"])
+@jwt_required()
+def update(id):
+    workout = get_workout(id)
+
+    # FIXME: add functionality for a GET request
+    data = request.get_json()
+    distance = data["distance"]
+    duration = data["duration"]
+    error = None
+
+    if not distance:
+        error = "Distance is required"
+
+    if error is not None:
+        return jsonify({"error": error})
+    else:
+        db = get_db()
+        db.execute(
+            "UPDATE workout SET distance = ?, duration = ?" " WHERE id = ?",
+            (distance, duration, id),
+        )
+        db.commit()
+        return redirect(url_for("workout.index"))
