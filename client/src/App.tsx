0cf9e021ef211e17a57af225248d1d12f3684023
Lucius Holzinger <luciholzinger@gmail.com>
2025-01-22 18:32:06 -0800

    The fetching of the data to the api has been modified to enforce tighter error checking to make it easier to detect errors.
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,13 @@ function App() {
   const [array, setArray] = useState([]);
 
   const fetchAPI = async () => {
-    const response = await axios.get("http://127.0.0.1:8080/api/users");
-    setArray(response.data.users);
+    axios.get("http://127.0.0.1:8080/api/users")
+      .then(response => {
+        setArray(response.data.users);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
   };
 
   useEffect(() => {
@@ -16,13 +21,19 @@ function App() {
 
   return (
     <>
-      <h1 className="text-5xl font-bold underline">Hello World!</h1>
-      {array.map((user, index) => (
-        <div key={index}>
-          <span>{user}</span>
-          <br/>
-        </div>
-      ))}
+      <h1 className="text-5xl font-bold underline p-4">Hello World!</h1>
+      {array ? (
+        <>
+          {array.map((user, index) => (
+            <div key={index}>
+              <span>{user}</span>
+              <br/>
+            </div>
+          ))}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
