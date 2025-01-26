c10b1bdae31f81e25cdd6ee697f1708d0e6d8e0a
Lucius Holzinger <luciholzinger@gmail.com>
2025-01-25 20:09:29 -0800

    The register form has been constructed. Only the necessary parts are included for the user to register. Haven't connected to the backend yet
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -3,8 +3,32 @@
 function Register() {
   return (
     <>
-      <h1>Register</h1>
-      <p>This is the register form page.</p>
+      <h2>Register</h2>
+      <form>
+        <div>
+          <label>Username</label>
+          <input type="text" id="username"/>
+        </div>
+
+        <div>
+          <label>Email</label>
+          <input type="email" />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input type="password" id="password"/>
+        </div>
+
+        <div>
+          <label>Confirm Password</label>
+          <input type="password" id="confirmPassword" />
+        </div>
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
     </>
   );
 }
