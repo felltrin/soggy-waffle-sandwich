c1be0ce21370754d3866c39ebf73d12ce7c39dd1
Lucius Holzinger <luciholzinger@gmail.com>
2025-03-25 16:07:44 -0700

    a hover effect has been put on the icons for each workout
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- client/src/components/Workout.tsx
+++ client/src/components/Workout.tsx
@@ -6,7 +6,7 @@ const Workout = ({ workout, index, setWorkoutUpdateId, onDelete }) => {
     <>
       <div
         key={index}
-        className="flex justify-between items-center bg-white py-3 px-4 rounded-md mb-4 cursor-pointer"
+        className="flex justify-between items-center bg-white py-3 px-4 rounded-md mb-4"
       >
         <span>{workout.created.substring(0, 16)} </span>
         <span>{workout.distance}km </span>
@@ -14,7 +14,7 @@ const Workout = ({ workout, index, setWorkoutUpdateId, onDelete }) => {
         <div className="flex items-center justify-start gap-x-4">
           <Link to="/update-log">
             <button>
-              <div className="bg-gray-200 dark:bg-gray-700 p-1 rounded-lg">
+              <div className="bg-gray-200 dark:bg-gray-700 p-1 rounded-lg transition-colors duration-200 hover:bg-gray-300 dark:hover:bg-gray-600 cursor-pointer">
                 <SquarePen
                   className="w-5 h-5 mx-2"
                   onClick={() => setWorkoutUpdateId(workout.id)}
@@ -23,7 +23,7 @@ const Workout = ({ workout, index, setWorkoutUpdateId, onDelete }) => {
             </button>
           </Link>
           <button>
-            <div className="bg-gray-200 dark:bg-gray-700 p-1 rounded-lg inline-flex">
+            <div className="bg-gray-200 dark:bg-gray-700 p-1 rounded-lg inline-flex transition-colors duration-200 hover:bg-gray-300 dark:hover:bg-gray-600 cursor-pointer">
               <Trash2
                 className="w-5 h-5 mx-2"
                 onClick={() => onDelete(workout.id)}
