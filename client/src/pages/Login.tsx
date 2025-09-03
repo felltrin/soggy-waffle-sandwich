e03a6db6f2d1088d729d8f937bea39059fb896bc
Lucius Holzinger <luciholzinger@gmail.com>
2025-09-03 11:35:15 -0700

    the login page and register page styling has been fixed. the two buttons in the hero also direct to the correct places
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -21,7 +21,6 @@ function Login() {
 
       const { access_token } = response.data;
       localStorage.setItem("token", access_token);
-      // alert(response.data.message);
       navigate("/");
       navigate(0);
     } catch (error) {
