77fe46fb42e13c014f6da2033cc2110ca4da33f0
Lucius Holzinger <luciholzinger@gmail.com>
2025-09-09 15:38:36 -0700

    updated the base url variable to include the prod url
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Experience from "./components/Experience.tsx";
 
 export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://127.0.0.1:8080" : "/";
+  import.meta.env.MODE === "development"
+    ? "http://127.0.0.1:8080"
+    : "https://soggy-waffle-sandwich.onrender.com";
 
 function App() {
   return (
